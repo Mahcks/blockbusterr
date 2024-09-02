e89c1eaed7cada3caeb5570381cfa4a1e6376c11
mahcks <max@mahcks.com>
2024-09-02 11:40:06 -0500

    Initial commit

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,13 @@
+import '@/index.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-red-500">Red Text</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ apps/server/go.mod
@@ -0,0 +1,3 @@
+module github.com/mahcks/blockbusterr
+
+go 1.22.0
