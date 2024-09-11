97b6fe232b6fb32914269e21f13118a3a02624f9
mahcks <max@mahcks.com>
2024-09-10 19:43:54 -0500

    client-feat: changed theme and added a SideNavBar

--- apps/client/src/routes/settings.tsx
+++ apps/client/src/routes/settings.tsx
@@ -1,107 +1,29 @@
-import * as React from "react";
-import NavBar from "@/components/NavBar";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import MovieSettings from "@/components/MovieSettings";
 import TVShowSettings from "@/components/TVShowSettings";
-
-const categories = [
-  {
-    value: "movies",
-    label: "Movies",
-  },
-  {
-    value: "tv-shows",
-    label: "TV Shows",
-  },
-];
+import SidebarNav from "@/components/SideBarNav";
+import { useState } from "react";
 
 export default function Settings() {
-  // Deals with the state of the settings category
-  const [openCategory, setOpenCategory] = React.useState(false);
-  const [settingsCategory, setSettingsCategory] = React.useState("movies");
-
-  // Conditionally render content based on settingsCategory
-  const renderSettingsPage = () => {
-    switch (settingsCategory) {
-      case "movies":
-        return <MovieSettings />;
-      case "tv-shows":
-        return <TVShowSettings />;
-      default:
-        return null;
-    }
-  };
+  // State to persist tab content
+  const [activeTab, setActiveTab] = useState("movies");
 
   return (
     <div>
-      <NavBar />
-
-      <div className="flex min-h-screen flex-col pr-5 pl-5">
-        <Popover open={openCategory} onOpenChange={setOpenCategory}>
-          <PopoverTrigger asChild>
-            <Button
-              variant="outline"
-              role="combobox"
-              aria-expanded={openCategory}
-              className="w-[200px] justify-between"
-            >
-              {settingsCategory
-                ? categories.find(
-                    (category) => category.value === settingsCategory
-                  )?.label
-                : "Select setting..."}
-              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-[200px] p-0">
-            <Command>
-              <CommandList>
-                <CommandEmpty>No setting found.</CommandEmpty>
-                <CommandGroup>
-                  {categories.map((category) => (
-                    <CommandItem
-                      key={category.value}
-                      value={category.value}
-                      onSelect={(currentValue) => {
-                        setSettingsCategory(
-                          currentValue === settingsCategory ? "" : currentValue
-                        );
-                        setOpenCategory(false);
-                      }}
-                    >
-                      <Check
-                        className={cn(
-                          "mr-2 h-4 w-4",
-                          settingsCategory === category.value
-                            ? "opacity-100"
-                            : "opacity-0"
-                        )}
-                      />
-                      {category.label}
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-              </CommandList>
-            </Command>
-          </PopoverContent>
-        </Popover>
-
-        {/* Render the settings page based on the selected category */}
-        <div className="mt-5">{renderSettingsPage()}</div>
+      <SidebarNav />
+      <div className="pl-60 pt-5">
+        <Tabs value={activeTab} onValueChange={setActiveTab} className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="movies">Movies</TabsTrigger>
+            <TabsTrigger value="shows">Shows</TabsTrigger>
+          </TabsList>
+          <TabsContent value="movies" hidden={activeTab !== "movies"}>
+            <MovieSettings />
+          </TabsContent>
+          <TabsContent value="shows" hidden={activeTab !== "shows"}>
+            <TVShowSettings />
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   );
