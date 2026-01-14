e99bdf93c6f4c9999258e49f2fa9e5e259cd78d7
mahcks <max@mahcks.com>
2026-01-13 20:17:57 -0600

    bug-fix: changed docs base path

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mahcks.github.io',
-	base: '/blockbusterr',
+	base: '/',
 	integrations: [
 		starlight({
 			title: 'Blockbusterr',
